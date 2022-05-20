4cacf4952140a62565165fff8b4524bcf7f22324
FahadwebD <fahadchowdhury66779@gmail.com>
2022-05-21 02:44:48 +0600

    fetch api by end point id

--- api.js
+++ api.js
@@ -1,12 +1,27 @@
 console.log('connected')
 
-const fetchingDetail =()=>{
+const fetchingDetail =(id)=>{
 
-
+   
     fetch(`https://gorest.co.in/public/v1/users/${id}`)
     .then(res=> res.json())
-    .then(data => console.log(data))
+    .then(data => showDetail(data))
 }
+const showDetail = (data)=>{
+    console.log(data.data)
+    const sec = document.getElementById('main');
+    const newItem = document.createElement('div')
+        newItem.innerHTML =`<div class="card-body">
+              <h5>${data.data.name}</h5>
+            
+        </div>`
+    sec.appendChild(newItem)
 
+}
+document.getElementById('search-btn').addEventListener('click' , function() {
+    
+    const inputV = document.getElementById('input-field').value
+    fetchingDetail(parseInt(inputV))
 
+})
 
